31285b772001f6cd8f0bbda2ad5afa692f7907d1
KamilWardyla <conax13@gmail.com>
2022-11-18 18:34:38 +0100

    third

--- InGoodHands/charity_donation/views.py
+++ InGoodHands/charity_donation/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
 from .models import Donation, Institution, User
 from django.urls import reverse_lazy
 from .forms import DonationForm, AddUserForm
